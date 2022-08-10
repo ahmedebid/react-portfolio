ffe2651cb2d5ff826a33931b466e3d6ddae96161
ahmedebid <ahmed-ebid@outlook.com>
2022-08-10 14:45:15 +0200

    Change the colors of the tech stack tags to increase its contrast

--- src/components/ProjectsSection/TechTag.js
+++ src/components/ProjectsSection/TechTag.js
@@ -3,15 +3,15 @@ export default function TechTag(props) {
     let tagColor;
 
     if (props.tech === "React") {
-        tagColor = "#38BDF8";
+        tagColor = "#044662";
     } else if (props.tech === "Rest API") {
-        tagColor = "#F43F5E";
+        tagColor = "#004E04";
     } else if (props.tech === "HTML") {
-        tagColor = "#E34C26";
+        tagColor = "#7A2510";
     } else if (props.tech === "CSS") {
-        tagColor = "#214CE5";
+        tagColor = "#1434A9";
     } else if (props.tech === "JavaScript") {
-        tagColor = "#FCDC00";
+        tagColor = "#4D4200";
     } else {
         tagColor = "#000";
     }
